ece3758d25ca9e9de9bc6629d6f2c6c981507fae
Kristian Paulino <kristianp@github.com>
2019-02-11 19:58:28 +0800

    setup server and client for the basic (Masih Polos)

---
+++ client/src/services/AuthenticationService.js
@@ -0,0 +1,12 @@
+import Api from '@services/Api'
+
+export default {
+  register (credential) {
+    return Api().post('register', credential)
+  }
+}
+
+// AuthenticationService.register({
+//   email: 'testing@gmail.com',
+//   password: '123456'
+// })
